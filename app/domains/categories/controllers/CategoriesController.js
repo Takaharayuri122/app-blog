69caca9ae255b907c2d378e11f4f00e87946775a
Yuri Carvalho <yuricarvalho48@gmail.com>
2022-01-05 23:01:23 -0300

    Configurações para deploy na Vercel renomear arquivos

--- app/domains/categories/controllers/CategoriesController.js
+++ app/domains/categories/controllers/CategoriesController.js
@@ -3,7 +3,7 @@ const express = require('express');
 // Express feature that allows you to create routes without the need to use the "app" variable
 const router = express.Router();
 // Import category model
-const Category = require('app/domains/categories/models/category');
+const Category = require('/app/domains/categories/models/category');
 // Import slugify
 const slugify = require('slugify');
 // Import Authenticate Middleware
